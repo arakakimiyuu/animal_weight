d5db6f358f68dda1a0b05aead731271375330290
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-04-07 13:41:39 +0000

    [Add]コメント機能途中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,20 @@
 class Public::CommentsController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    comment = current_customer.comments.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(post)
+  end
+
+  def destroy
+    comment.destroy
+    redirect_to post_path(post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment, :customer_id, :post_id)
+  end
 end
