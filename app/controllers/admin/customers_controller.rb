914195359eb3f2853ea921caaf10920b318c7274
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-04-11 06:17:13 +0000

    [Add]管理者のcustomerページ作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,32 @@
 class Admin::CustomersController < ApplicationController
+
   def index
+    @customer = current_customer
+    @customers = Customer.all.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+      flash[:notice] = "変更を保存しました。"
+    else
+      render:edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :is_delete)
   end
 
 end
