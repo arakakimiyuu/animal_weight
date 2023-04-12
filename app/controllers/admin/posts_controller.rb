356f3e9caf32ea69a0a2df8a5f85791b036a7349
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-04-12 06:49:08 +0000

    [Add]管理者の投稿削除完了

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,19 +1,15 @@
 class Admin::PostsController < ApplicationController
 
+  before_action :authenticate_admin!
+
   def index
     @posts = Post.all.page(params[:page]).per(10)
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to admin_posts_path
+    Post.find(params[:id]).destroy
     flash[:notice] = "削除に成功しました。"
+    redirect_to admin_posts_path
   end
 
-  private
-
-  def post_params
-    params.require(:post).permit(:date, :weight, :feed, :amount_food, :customer_id, :pet_id)
-  end
 end
