581626359af15b21c90d4abee36a30c69687a752
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-04-15 04:51:04 +0000

    [update]いいね、コメント非同期通信化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -9,13 +9,15 @@ class Public::CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     comment = current_customer.comments.new(comment_params)
     comment.post_id = @post.id
+
     comment.save
     @comments = @post.comments.page(params[:page]).per(10)
     render:comment
   end
 
   def destroy
-    comment = PostComment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    comment = Comment.find(params[:id])
     comment.destroy
 
     @comments = @post.comments.page(params[:page]).per(10)
@@ -24,9 +26,9 @@ class Public::CommentsController < ApplicationController
 
   #投稿者 = 現在ログインしている会員でない場合
   def ensure_current_customer
-    @post = Post.find(params[:id])
-    unless @post.customer == current_customer
-     redirect_to post_path(@post)
+    comment = Comment.find(params[:id])
+    unless comment.customer == current_customer
+    redirect_to post_path(comment.post)
     end
   end
 
